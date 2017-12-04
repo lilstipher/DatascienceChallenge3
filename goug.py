87e3d89f6e947c8d13114a6002f08266af16d9aa
Lilstipher <sirmichel1995@gmail.com>
2017-12-04 16:59:31 +0100

    Nouvelle version

--- goug.py
+++ goug.py
@@ -1,26 +1,18 @@
-from skimage import data, segmentation,io
 from skimage.future import graph
+from skimage import data, segmentation, color, filters, io
 from matplotlib import pyplot as plt
 
 
-img = data.load('/home/hmkouassi/Bureau/challenge 3/DatascienceChallenge3/azeer.jpg')
-labels = segmentation.slic(img, compactness=30, n_segments=400)
-g = graph.rag_mean_color(img, labels)
+img = data.coffee()
+gimg = color.rgb2gray(img)
 
-fig, ax = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(6, 8))
+labels = segmentation.slic(img, compactness=30, n_segments=500)
+edges = filters.sobel(gimg)
+edges_rgb = color.gray2rgb(edges)
 
-ax[0].set_title('RAG drawn with default settings')
-lc = graph.show_rag(labels, g, img, ax=ax[0])
-# specify the fraction of the plot area that will be used to draw the colorbar
-fig.colorbar(lc, fraction=0.03, ax=ax[0])
+g = graph.rag_boundary(labels, edges)
+lc = graph.show_rag(labels, g, edges_rgb, img_cmap=None, edge_cmap='viridis',
+                    edge_width=1.2)
 
-ax[1].set_title('RAG drawn with grayscale image and viridis colormap')
-lc = graph.show_rag(labels, g, img,
-                    img_cmap='gray', edge_cmap='viridis', ax=ax[1])
-fig.colorbar(lc, fraction=0.03, ax=ax[1])
-
-for a in ax:
-    a.axis('off')
-
-plt.tight_layout()
-plt.show()
+plt.colorbar(lc, fraction=0.03)
+io.show()
